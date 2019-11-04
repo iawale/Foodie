b43cdc45d479b3e034f76c938275c74fe25acd6a
chiragawale <chiragawale777@gmail.com>
2019-11-03 21:19:37 -0600

    Expandable recyler view

--- app/src/main/java/com/chiragawale/foodie/FoodLogAdapter.java
+++ app/src/main/java/com/chiragawale/foodie/FoodLogAdapter.java
@@ -4,6 +4,7 @@ import android.content.Context;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.recyclerview.widget.RecyclerView;
@@ -31,9 +32,23 @@ public class FoodLogAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
         }
 
         @Override
-        public void onBindViewHolder(RecyclerView.ViewHolder holder, final int position) {
+        public void onBindViewHolder(final RecyclerView.ViewHolder holder, final int position) {
             ((ViewHolderMyLife) holder).title.setText(realmFoodList.get(position).getName());
-            }
+            ((ViewHolderMyLife) holder).calories.setText(realmFoodList.get(position).getCalories() +  "");
+            ((ViewHolderMyLife) holder).carbs.setText(realmFoodList.get(position).getCarbs() + "");
+            ((ViewHolderMyLife) holder).protein.setText(realmFoodList.get(position).getProtein() + "");
+            ((ViewHolderMyLife) holder).fat.setText(realmFoodList.get(position).getTotalFat() + "");
+            ((ViewHolderMyLife) holder).ll_food.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    int visibility = ((ViewHolderMyLife) holder).ll_food_detail.getVisibility();
+                    if(visibility == View.VISIBLE)
+                        ((ViewHolderMyLife) holder).ll_food_detail.setVisibility(View.GONE);
+                    else
+                        ((ViewHolderMyLife) holder).ll_food_detail.setVisibility(View.VISIBLE);
+                }
+            });
+        }
 
             // Return the size of your dataset (invoked by the layout manager)
             @Override
@@ -42,11 +57,18 @@ public class FoodLogAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder
             }
 
         class ViewHolderMyLife extends RecyclerView.ViewHolder {
-            TextView title;
+            TextView title, calories, protein, carbs, fat;
+            LinearLayout ll_food, ll_food_detail;
 
             public ViewHolderMyLife(View itemView) {
                 super(itemView);
                 title = itemView.findViewById(R.id.tv_food_name);
+                calories = itemView.findViewById(R.id.tv_food_calories);
+                protein = itemView.findViewById(R.id.tv_food_protein);
+                carbs = itemView.findViewById(R.id.tv_food_carbs);
+                fat = itemView.findViewById(R.id.tv_food_fat);
+                ll_food = itemView.findViewById(R.id.ll_food);
+                ll_food_detail = itemView.findViewById(R.id.ll_food_detail);
             }
 
         }
