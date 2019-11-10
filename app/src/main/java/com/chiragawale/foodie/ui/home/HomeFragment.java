d42140e1c0bcf843109b6970d329d63a5e487202
chiragawale <chiragawale777@gmail.com>
2019-11-09 19:49:49 -0600

    Clean up

--- app/src/main/java/com/chiragawale/foodie/ui/home/HomeFragment.java
+++ app/src/main/java/com/chiragawale/foodie/ui/home/HomeFragment.java
@@ -5,15 +5,11 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.fragment.app.FragmentManager;
-import androidx.fragment.app.FragmentPagerAdapter;
 import androidx.lifecycle.ViewModelProviders;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.viewpager.widget.ViewPager;
 
-import com.chiragawale.foodie.FoodLogAdapter;
 import com.chiragawale.foodie.R;
 import com.chiragawale.foodie.model.RealmFoodEntry;
 import com.chiragawale.foodie.ui.base.BaseFragment;
