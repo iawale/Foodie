9fe6fc6fa3a53c08c0c63c04717f65b7cba1f516
chiragawale <chiragawale777@gmail.com>
2019-11-16 16:11:36 -0600

    Show Calorie Amount

--- app/src/main/java/com/chiragawale/foodie/ui/dashboard/DashboardFragment.java
+++ app/src/main/java/com/chiragawale/foodie/ui/dashboard/DashboardFragment.java
@@ -1,28 +1,19 @@
 package com.chiragawale.foodie.ui.dashboard;
 
-import android.animation.Animator;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
-import android.widget.Spinner;
 import android.widget.TextView;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
-import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
 import androidx.lifecycle.ViewModelProviders;
 
 import com.chiragawale.foodie.R;
-import com.chiragawale.foodie.dao.ProgressDao;
 import com.chiragawale.foodie.ui.base.BaseFragment;
 import com.robinhood.spark.SparkView;
-import com.robinhood.spark.animation.LineSparkAnimator;
 import com.robinhood.spark.animation.MorphSparkAnimator;
-import com.robinhood.spark.animation.SparkAnimator;
 
 public class DashboardFragment extends BaseFragment {
 
@@ -35,11 +26,19 @@ public class DashboardFragment extends BaseFragment {
         View root = inflater.inflate(R.layout.fragment_dashboard, container, false);
         SparkView sparkView = root.findViewById(R.id.sparkview);
         BriefAdapter mAdapter = new BriefAdapter(progressDao.getCalorieData(foodDao.getAllFood()));
+        TextView tv_calView = root.findViewById(R.id.tv_cal);
         sparkView.setAdapter(mAdapter);
         // set animator
         MorphSparkAnimator morphSparkAnimator = new MorphSparkAnimator();
         morphSparkAnimator.setDuration(2000L);
         sparkView.setSparkAnimator(morphSparkAnimator);
+        sparkView.setScrubListener(value -> {
+            if (value == null) {
+                Toast.makeText(getContext(),"No value", Toast.LENGTH_SHORT).show();
+            } else {
+                tv_calView.setText("Calories: " + value);
+            }
+        });
         return root;
     }
 }
