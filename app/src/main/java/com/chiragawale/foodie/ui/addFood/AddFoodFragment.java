f3493cb518d1bdde1b526888ed1616ae84183aab
chiragawale <chiragawale777@gmail.com>
2020-02-01 18:59:57 -0600

    Add textwatcher in add food activity

--- app/src/main/java/com/chiragawale/foodie/ui/addFood/AddFoodFragment.java
+++ app/src/main/java/com/chiragawale/foodie/ui/addFood/AddFoodFragment.java
@@ -1,9 +1,14 @@
 package com.chiragawale.foodie.ui.addFood;
 
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -20,6 +25,7 @@ public class AddFoodFragment extends Fragment {
     private static final String ARG_SECTION_NUMBER = "section_number";
 
     private PageViewModel pageViewModel;
+    private EditText et_search;
 
     public static AddFoodFragment newInstance(int index) {
         AddFoodFragment fragment = new AddFoodFragment();
@@ -45,6 +51,23 @@ public class AddFoodFragment extends Fragment {
             @NonNull LayoutInflater inflater, ViewGroup container,
             Bundle savedInstanceState) {
         View root = inflater.inflate(R.layout.fragment_add_food, container, false);
+        et_search = root.findViewById(R.id.et_search);
+        et_search.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+//                Log.e("Ay", "New text:" + s);
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
         return root;
     }
 }
